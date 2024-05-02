4b31e340ae1dc74db648671c421fb0fa29cbb8b2
leoncepromus <twayinganyiki.l@gmail.com>
2024-05-02 15:39:00 +0000

    add README

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Fetch
-https://intranet.hbtn.io/status
-using urlib package
 """
-
+Fetch https://intranet.hbtn.io/status using the urllib package.
+"""
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+    # Create a request object with a user-agent header
+    req = urllib.request.Request(
+        'https://intranet.hbtn.io/status',
+        headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    )
+
+    with urllib.request.urlopen(req) as request:
         response = request.read()
         print("Body response:")
         print("\t- type: {}".format(type(response)))
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 readme file added for 0x09 pythhon object project
+make web move by 0x15-javascript-web_jquery
